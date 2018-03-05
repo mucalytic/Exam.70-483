e955d87c734e68b18a7ae91bbc59191f4942ce5b
Aaron Pina <apina@meddbase.com>
2018-03-05 14:48:50 +0000

    Did another listing

---
+++ Objective.1.4/Listing.1.79/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Listing_1_79
+{
+    public class Program
+    {
+        private delegate int Calculate(int x, int y);
+
+        // lambda expression to create a delegate
+        public static void Main(string[] args)
+        {
+            Calculate calc = (x, y) => x + y;
+
+            Console.WriteLine(calc(3, 4)); // 7
+
+            calc = (x, y) => x * y;
+
+            Console.WriteLine(calc(3, 4)); // 12
+            Console.ReadKey();
+        }
+    }
+}
