f164cbb15848f87bbbee2f19b885867cbe80b892
Aaron Pina <apina@meddbase.com>
2018-03-06 14:14:41 +0000

    Added some more listings

---
+++ Objective.1.4/Listing.1.87/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Listing_1_87
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pub = new Pub();
+
+            pub.OnChange += (s, a) => Console.WriteLine("Subscriber 1 called");
+            pub.OnChange += (s, a) => throw new Exception();
+            pub.OnChange += (s, a) => Console.WriteLine("Subscriber 3 called");
+
+            try
+            {
+                pub.Raise();
+            }
+            catch (AggregateException exception)
+            {
+                Console.WriteLine(exception.InnerExceptions.Count);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Pub
+    {
+        public event EventHandler OnChange;
+
+        public void Raise()
+        {
+            var exceptions = new List<Exception>();
+
+            foreach (var handler in OnChange?.GetInvocationList() ?? new Delegate[] {})
+            {
+                try
+                {
+                    handler.DynamicInvoke(this, EventArgs.Empty);
+                }
+                catch (Exception exception)
+                {
+                    exceptions.Add(exception);
+                }
+            }
+
+            if (exceptions.Any())
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+}
---
+++ Objective.1.5/Listing.1.92/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Listing_1_92
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+
+            try
+            {
+                var i = int.Parse(s);
+                if (i == 42)
+                {
+                    Environment.FailFast("Special number entered");
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Program complete");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
