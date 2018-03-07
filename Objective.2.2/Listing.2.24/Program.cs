f9190422adcf1a35508f57483c9f75603b048b43
Aaron <mucalytic@hotmail.com>
2018-03-07 08:16:57 +0000

    Did more listings

---
+++ Objective.2.2/Listing.2.24/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Listing_2_24
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var money = new Money(12.5m);
+            decimal whole = money;
+            var rounded = (int)money;
+
+            Console.WriteLine($"{whole} {rounded}");
+            Console.ReadKey();
+        }
+
+        public class Money
+        {
+            public decimal Amount { get; set; }
+
+            public static implicit operator decimal(Money money)
+            {
+                return money.Amount;
+            }
+
+            public static explicit operator int(Money money)
+            {
+                return (int)money.Amount;
+            }
+
+            public Money(decimal amount)
+            {
+                Amount = amount;
+            }
+        }
+    }
+}
